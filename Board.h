14a914e12636f6484cee69ba4586479320dab5d9
jclarke67 <jclarke@hamilton.edu>
2017-10-09 17:48:57 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,23 +1,22 @@
-#ifndef BOARD_H
-#define BOARD_H
-#include "Stack.h"
+#ifndef sudokuboard_H
+#define sudokuboard_H
+#include "stack.h"
 
 #include <iostream>
 
 using namespace std;
 
-class Board {
+class sudokuboard {
 public:
-  Board();
-  void BoardSetup(int r, int c, char n);
-  void place(Stack& stack, int r, int c, char n);    // place numeral n at position (r,c)
+  sudokuboard();
+  void place(stack& stack, int r, int c, int n);    // place numeral n at position (r,c)
+  void replace(int r, int c, char a);
   void print();                       // write the board to cout
   int get(int r, int c);              // return the numeral at position (r,c)
   void remove(int r, int c);          // remove the numeral at position (r,c)
-  bool canPlace(int r, int c, int n); // true iff the board would allow placing                                        
-                                      // n at (r,c).
-  bool solved();                      // true if there are no blank spots on the                                       
-                                      // board.
+  bool canPlace(int r, int c, int n); // true iff the board would allow placing                                        // n at (r,c).
+  bool solved();                      // true if there are no blank spots on the   
+  bool empty(int r, int c);                                    // board.
   void Build();
   void setRow(int row);
   void setCol(int col);
@@ -25,16 +24,18 @@ public:
   int getRow();
   int getCol();
   int getNum();
-  bool hasRowDuplicate(int c, int num);
-  bool hasColDuplicate(int r, int num);
-  bool hasBoxDuplicate(int r, int c, int num);
   char numToChar(int num);
+
 
 private:
   char grid[9][9];
   int Row;
   int Col;
   int Num;
+  bool hasRowDuplicate(int c, int num);
+  bool hasColDuplicate(int r, int num);
+  bool hasBoxDuplicate(int r, int c, int num);
+
     
 };
 
